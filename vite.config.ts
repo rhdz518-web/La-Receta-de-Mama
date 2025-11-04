09ea13499fd777b02a8d73f54689a0d4d6347678
rhdz518-web <r.h.d.z518@gmail.com>
2025-11-03 23:04:57 -0600

    Change base path in vite.config.ts
    Updated the base path for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tortilleria-pos/', // IMPORTANT: Replace 'tortilleria-pos' with your GitHub repository name
+  base: '/la receta de mama/', // IMPORTANT: Replace 'tortilleria-pos' with your GitHub repository name
 })
