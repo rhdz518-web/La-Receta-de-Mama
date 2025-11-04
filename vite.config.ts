6fa6b3dbdf27b887004d71646eeefc30c5786b55
rhdz518-web <r.h.d.z518@gmail.com>
2025-11-04 05:59:53 +0000

    Versión inicial del proyecto con Vite

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/la receta de mama/', // IMPORTANT: Replace 'tortilleria-pos' with your GitHub repository name
-})
